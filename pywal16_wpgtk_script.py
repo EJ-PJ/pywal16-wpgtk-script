2e4bf9b809e3cb2848a4c059e82cc7a0f13454ac
Josh <jrsosar01@gmail.com>
2025-01-26 10:17:25 -0600

    adding error checkin, wal command

--- pywal16_wpgtk_script.py
+++ pywal16_wpgtk_script.py
@@ -1,15 +1,36 @@
 import wpgtk
 import subprocess
 import re
+import sys
 
-WALL_DIR = wpgtk.data.config.WALL_DIR
-WALL_NAME = str(subprocess.check_output(['wpg', '-c']))
 
-WALL_FILE =  WALL_DIR + '/' + WALL_NAME
+def get_wal_arg():
 
-PARSE_WALL_FILE = re.findall(r"'(.*?)'", WALL_FILE)
+    WAL_ARG = sys.argv
 
-print("WALL DIR: ", type(WALL_DIR))
-print("WALL NAME: ", type(WALL_NAME))
-print("WALL FILE: ", WALL_FILE)
-print("PARSE WALL FILE: ", PARSE_WALL_FILE)
+    if((len(WAL_ARG) == 1) or (len(WAL_ARG) > 2)):
+        print("pywal16_wpgtk_script:", error_output())
+        sys.exit(0)
+
+    return WAL_ARG[1]
+
+def error_output():
+
+    err_msg = "error: Too many or very few arguments"
+    return err_msg
+
+def exec_wal():
+
+    WALL_DIR = wpgtk.data.config.WALL_DIR
+    WALL_NAME = subprocess.check_output(['wpg', '-c'])
+
+    DECODE_WALL_NAME = WALL_NAME.decode("utf-8").strip() #Decodes WALL_NAME since it cames as an object of type 'byte'
+
+    WALL_FILE =  WALL_DIR + '/' + DECODE_WALL_NAME
+
+    subprocess.run(["wal", "-i", WALL_FILE, "--cols16", get_wal_arg()])
+
+def main():
+    exec_wal()
+
+main()
