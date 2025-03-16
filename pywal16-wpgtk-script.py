00a4770494f17fafc3bff677c26802cfccec4505
Josh <jrsosar01@gmail.com>
2025-03-16 11:16:14 -0600

    changes in manage error output

--- pywal16-wpgtk-script.py
+++ pywal16-wpgtk-script.py
@@ -6,29 +6,31 @@ def get_wal_arg():
 
     WAL_ARG = sys.argv
 
-    if((len(WAL_ARG) == 1) or (len(WAL_ARG) > 2)):
-        print("pywal16_wpgtk_script:", error_output(error_num=1))
+    if (len(WAL_ARG) == 1) or (len(WAL_ARG) > 2):
+        error_output(error_num=1)
         sys.exit(0)
 
-    if((WAL_ARG[1] == "--darken") or ((WAL_ARG[1] == "-d"))):
+    if (WAL_ARG[1] == "--darken") or (WAL_ARG[1] == "-d"):
         print("pywal16_wpgtk_script: using 16 color output 'darken'\n")
         return "darken"
-    elif(WAL_ARG[1] == "--lighten" or ((WAL_ARG[1] == "-l"))):
+    elif WAL_ARG[1] == "--lighten" or (WAL_ARG[1] == "-l"):
         print("pywal16_wpgtk_script: using 16 color output 'lighten'\n")
         return "lighten"
     else:
-        print("pywal16_wpgtk_script:", error_output(error_num=2, wrong_arg=WAL_ARG[1]))
-        sys.exit(0)
+        error_output(error_num=2, wrong_arg=WAL_ARG[1])
 
 def error_output(error_num, wrong_arg=''):
 
+    ERR_MSG_PYWL = "pywal16_wpgtk_script"
     ERR_MSG_1 = "error: Too many or very few arguments"
     ERR_MSG_2 = f"error: '{wrong_arg}' Is not a valid argument"
 
-    if(error_num == 1):
-        return ERR_MSG_1
-    elif(error_num == 2):
-        return ERR_MSG_2
+    if error_num == 1:
+        print(ERR_MSG_PYWL, ERR_MSG_1)
+        sys.exit(0)
+    elif error_num == 2:
+        print(ERR_MSG_PYWL, ERR_MSG_2)
+        sys.exit(0)
 
 def exec_wal():
 
